f709d6dbaec83515532d2968c4cf94192c2d5644
vcorneau <vcorneau@gmail.com>
2023-06-27 16:46:41 -0400

    Add number of pips

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-body {
-  
+function Deck(swampPip, forestPip, islandPip, mountainPip, plainsPip) {
+  this.swampPip = swampPip;
+  this.forestPip = forestPip;
+  this.islandPip = islandPip;
+  this.mountainPip = mountainPip;
+  this.plainsPip = plainsPip;
 }
+
+
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", submitCalc);
+
+function sumPips(swampPip, forestPip, islandPip, mountainPip, plainsPip) {
+  let total = swampPip + forestPip + islandPip + mountainPip + plainsPip;
+  return total;
+}
+
+function displayTotal(total) {
+  const pipDisplay = document.getElementById("totalPip");
+  pipDisplay.innerHTML = "Total Pips : " + total;
+}
+
+function submitCalc(event) {
+  event.preventDefault();
+  document.getElementsByClassName("pipCount");
+  let swampPip = parseInt(swampPipCount.value);
+  let forestPip = parseInt(forestPipCount.value);
+  let islandPip = parseInt(islandPipCount.value);
+  let mountainPip = parseInt(mountainPipCount.value);
+  let plainsPip = parseInt(plainsPipCount.value);
+  let newDeck = new Deck(swampPip, forestPip, islandPip, mountainPip, plainsPip);
+  let total = sumPips(swampPip, forestPip, islandPip, mountainPip, plainsPip);
+  console.log(newDeck);
+  displayTotal(total);
+}
+
